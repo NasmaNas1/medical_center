390333316848f3a4bdf3619716443ba41ea7c507
NasmaNas1 <nasmaaina@gmail.com>
2025-08-13 15:02:24 +0300

    duration

--- app/Http/Resources/DoctorScheduleResource.php
+++ app/Http/Resources/DoctorScheduleResource.php
@@ -18,7 +18,7 @@ class DoctorScheduleResource extends JsonResource
             'id' => $this->id,
             'doctor_id' => $this->doctor_id,
             'sub_specialization'=>$this->subspecialization->name ?? null,
-            'day' => $this->day,
+            'duration' => $this->subspecialization->duration ?? null,             'day' => $this->day,
             'start_time' => $this->start_time,
             'end_time' => $this->end_time,
             'is_available' => $this->is_available,
